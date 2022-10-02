e44eb341c0ac2965853d6e996440218eda42c533
Masaya Kajita <kjtech2020@gmail.com>
2022-10-02 11:26:55 +0900

    add

---
+++ formula9.js
@@ -0,0 +1,18 @@
+let R = 75;
+function setup() {
+ createCanvas(400, 400);
+ noLoop();
+}
+function draw() {
+ background(235);
+ fill(0, 0, 200);
+ noStroke();
+ push();
+ translate(width / 2, height / 2);
+ for (let i = 0; i < 999; i += 0.01) {
+   let A = sin(i) + cos(i * 2) + 1.5;
+   let B = (i + cos(i * 2) - cos(i * 3)) / 6;
+   circle(R * A * cos(B), R * A * sin(B), 10 * sin(i * 5));
+}
+ pop();
+}
