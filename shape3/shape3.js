84745a98d4582722afaf272f97af057d1adb6e36
Masaya Kajita <kjtech2020@gmail.com>
2022-10-05 01:09:18 +0900

    add shape

---
+++ shape3/shape3.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    noLoop();
+  }
+  
+  function draw() {
+    background(220);
+    noFill();
+    beginShape();
+    for (let i = 0; i < width; i+=5) {
+    vertex(i, random(height * 0.25, height * 0.75));
+    }
+    endShape();  
+  }
---
+++ shape4/shape4.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    noLoop();
+  }
+  
+  function draw() {
+    background(220);
+    noFill();
+    beginShape();
+   for (let i = 0; i < width; i++) {
+   let x = i;
+   let y = map(noise(i * 0.01), 0, 1, height * 0.25, height * 0.75);
+   vertex(x, y)
+   }
+    endShape();  
+  }
